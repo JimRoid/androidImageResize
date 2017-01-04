abf604bc6c12fac241e986925fae2c103d59eb2e
EasyApp_Jim <jim@easyapp.com.tw>
2017-01-04 12:05:47 +0800

    fix bug

--- lib/src/main/java/com/easyapp/imageresize/operations/ImageResize.java
+++ lib/src/main/java/com/easyapp/imageresize/operations/ImageResize.java
@@ -2,11 +2,14 @@ package com.easyapp.imageresize.operations;
 
 import android.content.res.Resources;
 import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
 
 import com.easyapp.imageresize.utils.ImageDecoder;
 import com.easyapp.imageresize.utils.ImageOrientation;
 
 import java.io.File;
+import java.io.IOException;
 
 
 public class ImageResize {
@@ -32,7 +35,15 @@ public class ImageResize {
 	}
 	
 	public static Bitmap resize(File original, int width, int height, ResizeMode mode) {
+		int degree = readPictureDegree(original.getAbsolutePath());
+
 		Bitmap sampledSrcBitmap = ImageDecoder.decodeFile(original, width, height);
+		Matrix matrix = new Matrix();
+		matrix.postRotate(degree); /*翻转90度*/
+		width = sampledSrcBitmap.getWidth();
+		height =sampledSrcBitmap.getHeight();
+		sampledSrcBitmap = Bitmap.createBitmap(sampledSrcBitmap, 0, 0, width, height, matrix, true);
+
 		if(sampledSrcBitmap == null) {
 			return null;
 		}
@@ -72,5 +83,32 @@ public class ImageResize {
 	private static int calculateHeight(int originalWidth, int originalHeight, int width) {
 		return (int) Math.ceil(originalHeight / ((double) originalWidth/width));
 	}
-	
+
+	/**
+	 * 读取照片exif信息中的旋转角度
+	 * @param path 照片路径
+	 * @return角度
+	 */
+	public static int readPictureDegree(String path) {
+		int degree  = 0;
+		try {
+			ExifInterface exifInterface = new ExifInterface(path);
+			int orientation = exifInterface.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+			switch (orientation) {
+				case ExifInterface.ORIENTATION_ROTATE_90:
+					degree = 90;
+					break;
+				case ExifInterface.ORIENTATION_ROTATE_180:
+					degree = 180;
+					break;
+				case ExifInterface.ORIENTATION_ROTATE_270:
+					degree = 270;
+					break;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return degree;
+	}
+
 }
